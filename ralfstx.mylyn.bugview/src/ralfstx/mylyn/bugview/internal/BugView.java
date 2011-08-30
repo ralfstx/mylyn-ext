e06cb9b8ad296c6fe0e46e8d3126a22a8ab3038f
Ralf Sternberg <ralfstx@googlemail.com>
2011-08-30 12:49:58 +0200

    Added Bugs view

---
+++ ralfstx.mylyn.bugview/src/ralfstx/mylyn/bugview/internal/BugView.java
@@ -0,0 +1,21 @@
+package ralfstx.mylyn.bugview.internal;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+public class BugView extends ViewPart {
+
+  @Override
+  public void createPartControl( Composite parent ) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void setFocus() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
